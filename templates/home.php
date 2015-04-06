d8e06b21d36e2e643f926dbb49c508bf881047cd
gebeer <gbr@gmx.net>
2015-04-06 16:19:02 +0700

    cleanup home.php

--- templates/home.php
+++ templates/home.php
@@ -1,16 +1,2 @@
 <?php 
-$useMain = true; 
 $content = $page->title;
-
-/*
- * Check that required settings are in place.
- * Feel free to remove this if your site already works.
- *
- */
-if($config->prependTemplateFile != '_init.php' || $config->appendTemplateFile != '_main.php') echo '
-	<h2>Please edit your /site/config.php and set:</h2>
-	<pre>
-	$config->prependTemplateFile = "_init.php";
-	$config->appendTemplateFile = "_main.php"; 
-	</pre>
-	';
